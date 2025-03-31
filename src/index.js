ffc934efa3c78f0c7c4b0d2b900d5866f7cd34e0
Sebastien Moraz <sebastien@moraz.net>
2025-03-31 16:39:54 +0200

    feat: add global search in WynnSDK

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import AbilityController from "./modules/AbilityController.js";
 import AspectController from "./modules/AspectController.js";
 import MapController from "./modules/MapController.js";
 import QuestController from "./modules/QuestController.js";
+import ApiCaller from "./modules/ApiCaller.js";
 
 globalThis.sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export default class WynnSDK {
 	constructor() {
+		this.api = ApiCaller.getInstance();
 		this.player = new PlayerController();
 		this.guild = new GuildController();
 		this.item = new ItemController();
@@ -19,6 +21,11 @@ export default class WynnSDK {
 		this.aspect = new AspectController();
 		this.map = new MapController();
 		this.quest = new QuestController();
+	}
+	
+	async search(query) {
+		const url = `${this.api.url}/search/${query}`;
+		const response = await this.api.request(url);
 	}
 }
 
